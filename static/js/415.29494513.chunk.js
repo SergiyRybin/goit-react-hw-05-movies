ab44e3b2707d0022cdfcd40f0820bec7156723e7
SergiyRybin <89793296+SergiyRybin@users.noreply.github.com>
2022-06-19 14:04:20 +0000

    Deploying to gh-pages from @ SergiyRybin/goit-react-hw-05-movies@75a63dcb3168d82573134c340fb3e0ca12935aef 🚀

---
+++ static/js/415.29494513.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);var n=r(885),s=r(599),u=r(700),a=r(665),c=r(791),l=r(184);t.default=function(){var e=(0,c.useState)([]),t=(0,n.Z)(e,2),r=t[0],h=t[1];(0,c.useEffect)((function(){0===r.length&&(0,a.ue)().then((function(e){var t=e.results;return h({results:t})}))}),[]);var f=r.results;return(0,l.jsxs)(u.Z,{children:[(0,l.jsx)("h1",{children:"Trending today"}),(0,l.jsx)(s.Z,{data:f})]})}}}]);
+//# sourceMappingURL=415.29494513.chunk.js.map
