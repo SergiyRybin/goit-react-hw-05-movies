2a7234cdb6c03605a1dbd7a70ddb6c13fcc2e984
SergiyRybin <serhiobax@gmail.com>
2022-06-20 16:49:34 +0300

    Update FilmCard.jsx

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -3,21 +3,17 @@ import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import iconNmae from 'images/2.jpeg';
 
-
-
 const FilmCard = ({ film }) => {
   const location = useLocation();
-
-  const noDataRender = {
-   
-    poster_path: iconNmae,
-    
-  };
 
   return (
     <Link to={`/movies/${film.id}`} state={location.search}>
       <img
-        src={film.poster_path ? `https://image.tmdb.org/t/p/w500/${film.poster_path}` : iconNmae}
+        src={
+          film.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${film.poster_path}`
+            : iconNmae
+        }
         alt=""
         className={s.ImageItem}
       />
